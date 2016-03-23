669348265bc82b74ddd6643d4d182da6b8b4a15d
Oleg_Kariakin <olegkariakin1988@gmail.com>
2016-03-23 12:09:21 +0200

    String  Fizz Buzz

---
+++ algo/src/main/java/StringFizzBuzz.java
@@ -0,0 +1,31 @@
+import java.util.StringTokenizer;
+
+/**
+ * User: Oleg_Kariakin
+ * Date: 3/23/16
+ */
+public class StringFizzBuzz {
+
+    public static String fizzBuzz(String initial, String third, String fifth, String fifteents) {
+        StringTokenizer tokenizer = new StringTokenizer(initial, " ,;:/?");
+        StringBuffer result = new StringBuffer();
+
+        int wordCounter = 0;
+        while (tokenizer.hasMoreTokens()) {
+            wordCounter++;
+            String word = tokenizer.nextToken();
+
+            if (wordCounter % 15 == 0) {
+                result.append(" ").append(fifteents);
+            } else if (wordCounter % 5 == 0) {
+                result.append(" ").append(fifth);
+            } else if (wordCounter % 3 == 0) {
+                result.append(" ").append(third);
+            } else {
+                result.append(" ").append(word);
+            }
+        }
+        return result.toString();
+    }
+
+}
---
+++ algo/src/test/java/StringFizzBuzzTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: Oleg_Kariakin
+ * Date: 3/23/16
+ */
+public class StringFizzBuzzTest {
+
+    @Test
+    public void testFizzBuzz() throws Exception {
+        //given
+        String initialString = " The string tokenizer class allows an application to break a string into tokens. The tokenization method is much simpler than the one used by the StreamTokenizer class. The StringTokenizer methods do not distinguish among identifiers, numbers, and quoted strings, nor do they recognize and skip comments.";
+        String expectedString = " The string third class fifth third application to third fifth string third tokens. The fifteenth method is third simpler fifth third one used third fifth StreamTokenizer third The StringTokenizer fifteenth do not third among fifth third and quoted third fifth do third recognize and fifteenth comments.";
+
+        //when
+        String result = StringFizzBuzz.fizzBuzz(initialString, "third", "fifth", "fifteenth");
+
+        //expected
+        assertEquals(expectedString, result);
+    }
+
+}
