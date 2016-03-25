ee4cf50e93a38f1b6fcba9e154b788f019ccfc0d
Oleg_Kariakin <olegkariakin1988@gmail.com>
2016-03-25 15:33:16 +0200

    Minor changes to bubble-sort

--- algo/src/main/java/sort/BubbleSort.java
+++ algo/src/main/java/sort/BubbleSort.java
@@ -4,13 +4,16 @@ package sort;
  * User: Oleg_Kariakin
  * Date: 3/24/16
  */
+//TODO - create an interface for sort for further implementations
+//TODO - create generic implementation to extend int[] to other Number values
+//TODO - create factory for different implementations of sorting
 public class BubbleSort {
 
     /**
      * Bubble sort - have n^2, shouldn't be used
-     * @param arrayToSort - array to be sorted
-     * @param ascending - true, false - descending
      *
+     * @param arrayToSort - array to be sorted
+     * @param ascending   - true, false - descending
      * @throws IllegalArgumentException
      */
     public static int[] sort(int[] arrayToSort, boolean ascending) throws IllegalArgumentException {
@@ -21,14 +24,13 @@ public class BubbleSort {
             for (int j = 0; j < arrayToSort.length - i - 1; j++) {
                 if (ascending) { //ascending
                     if (arrayToSort[j] > arrayToSort[j + 1]) {
-                        swap(arrayToSort, j, j+1);
+                        swap(arrayToSort, j, j + 1);
                     }
                 } else { //descending
                     if (arrayToSort[j] < arrayToSort[j + 1]) {
-                        swap(arrayToSort, j, j+1);
+                        swap(arrayToSort, j, j + 1);
                     }
                 }
-
             }
         }
 
